723edd99302aa25f8316d69b428fe6c70f606d6d
Yura Oleksyshyn <yuraolex@gmail.com>
2018-09-04 18:20:15 +0300

    Added modules for boost and libbitcoin. Added simple test for libbitcoin

---
+++ modules/libbitcoin/libbitcoin.qbs
@@ -0,0 +1,25 @@
+// Copyright (c) 2018 The XSN developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+import qbs
+
+Module {
+//    name: "vendor"
+    Depends { name: "cpp" }
+    Depends { name: "boost" }
+
+    property string includePath: path
+    property string libFolder: includePath + "/lib/"
+
+    cpp.includePaths: [
+        includePath + "/include/bitcoin",
+        includePath + "/include",
+    ]
+
+    cpp.staticLibraries: [
+        libFolder + "libbitcoin.a",
+        libFolder + "libsecp256k1.a",
+        "gmp"
+    ]
+}
--- src/tests/core_tests/tst_coretests.hpp
+++ src/tests/core_tests/tst_coretests.hpp
@@ -3,6 +3,8 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #include <gtest/gtest.h>
 #include <gmock/gmock-matchers.h>
+#include <bitcoin/bitcoin.hpp>
+#include <iostream>
 
 using namespace testing;
 
@@ -10,4 +12,14 @@ TEST(CoreTests, CoreTests)
 {
     EXPECT_EQ(1, 1);
     ASSERT_THAT(0, Eq(0));
+}
+
+TEST(CoreTests, LibbitcoinTest)
+{
+    const auto block = bc::chain::block::genesis_mainnet();
+    const auto& tx = block.transactions().front();
+    const auto& input = tx.inputs().front();
+    const auto script = input.script().to_data(false);
+    std::string message(script.begin() + sizeof(uint64_t), script.end());
+    std::cout << message << std::endl;
 }
